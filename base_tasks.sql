6b0c5b2df9d91a357dbd72e1a8f880b42b028faa
Brenda-Landa <Brenda Landa>
2024-11-27 10:34:47 -0600

    Cambio en los tipos de la base de datos

--- base_tasks.sql
+++ base_tasks.sql
@@ -1,11 +1,11 @@
 DROP TABLE IF EXISTS `tasks`;
 CREATE TABLE `tasks` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `title` varchar(45) NOT NULL,
   `descripcion` varchar(255) DEFAULT NULL,
   `status` varchar(45) NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) 
 
