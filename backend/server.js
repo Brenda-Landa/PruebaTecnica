2a860e967816382c1475cf3c5a3452b398c7324e
Brenda-Landa <Brenda Landa>
2024-11-28 00:52:32 -0600

    función para crear una nueva tarea

--- backend/server.js
+++ backend/server.js
@@ -5,17 +5,17 @@ const cors = require('cors');
 const {Pool} = require('pg');
 require('dotenv').config();
 
-//Creación de una plicación Express
+//Creación de una aplicación Express
 const app = express();
 
 //Configuración de la configuración con la base de datos 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
     database: process.env.DB_NAME,
-});
+    password: process.env.DB_PASSWORD,
+    port: process.env.PORT,
+  });
 
 //Aplicación de middlewares
 app.use(cors());
@@ -23,8 +23,6 @@ app.use(bodyParser.json());
 
 //Puerto en el que va a correr el servidor 
 const PORT = process.env.PORT || 3000;
-
-// En esta parte se subirán las rutas 
 
 //Devuelve la lista de todas las tareas
 app.get('/api/tasks', async (req, res) => {
@@ -36,6 +34,20 @@ app.get('/api/tasks', async (req, res) => {
     }
   });
 
+  //Creación de una nueva tarea 
+  app.post('/api/tasks', async (req, res) => {
+    const { title, description, status } = req.body;
+    try {
+        const result = await pool.query(
+            'INSERT INTO tasks (title, description, status) VALUES ($1, $2, $3) RETURNING *',
+            [title, description, status]
+        );
+        res.status(201).json(result.rows[0]);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
 //Eliminar una tarea existente 
 app.delete('/api/tasks/:id', async (req, res) => {
     const {id} = req.params;
@@ -46,10 +58,11 @@ app.delete('/api/tasks/:id', async (req, res) => {
         res.status(500).send('Error en la eliminación de la tarea');
     }
 
-}
-)
+});
+
+
 
 //Se inicia el puerto y se escucha en el puerto ya establecido 
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    console.log(`El servidor está corriendo en http://localhost:${PORT}`);
 });
