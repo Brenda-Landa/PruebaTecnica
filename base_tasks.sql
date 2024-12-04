a8b9fd0ae21a4f3467d8bc7ae3fbdd38fd9569bf
Brenda-Landa <112039342+Brenda-Landa@users.noreply.github.com>
2024-12-03 22:41:42 -0600

    Update base_tasks.sql
    Cambio en descripcion

--- base_tasks.sql
+++ base_tasks.sql
@@ -2,10 +2,9 @@ DROP TABLE IF EXISTS `tasks`;
 CREATE TABLE `tasks` (
   `id` int NOT NULL AUTO_INCREMENT,
   `title` varchar(45) NOT NULL,
-  `descripcion` varchar(255) DEFAULT NULL,
+  `descrpcion` varchar(255) DEFAULT NULL,
   `status` varchar(45) NOT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) 
-
