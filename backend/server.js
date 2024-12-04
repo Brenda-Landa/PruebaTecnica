2fb1f0e4b6814b2123773348903f9fa9a075e49c
Brenda-Landa <Brenda Landa>
2024-12-03 21:24:32 -0600

    funcion para encontrar una tarea por su id

--- backend/server.js
+++ backend/server.js
@@ -40,14 +40,16 @@ app.get('/api/tasks', async (req, res) => {
 //Devolución de una tarea específica
 app.get('/api/tasks/:id', async (req, res) => {
   const{id} = req.params;
-  try{
-    const result = await pool.query('SELECT * FROM tasks WHERE id = $1', [id]);
-    if(result.rows.length == 0){
-      res.status(404).json('Tarea no encontrada');
+  try {
+    const [rows] = await pool.query('SELECT * FROM tasks WHERE id = ?', [id]);
+    console.log('Resultado de la consulta:', rows);
+    if (rows.length === 0) {
+      return res.status(404).json({ error: 'Tarea no encontrada' });
     }
-    res.status(200).json(result.rows[0]);
-  }catch(error){
-    res.status(500).json('Error en la selección de tarea');
+    return res.status(200).json(rows[0]);
+  } catch (error) {
+    console.error('Error al seleccionar la tarea:', error);
+    return res.status(500).json({ error: 'Error en la selección de tarea' });
   }
 });
 
@@ -71,7 +73,7 @@ app.put('/api/tasks/:id', async (req, res) => {
   const{status} = req.body; 
   try{
     const result = await pool.query(
-      "UPDATE tasks SET status = $1, updated_at = NOW() WHERE id = $2 RETURNING *",
+      "UPDATE tasks SET status = ?, updated_at = NOW() WHERE id = ? RETURNING *",
       [status, id]
     );
     if(result.rows.length === 0){
